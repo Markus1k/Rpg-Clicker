2aa586c72a2f146980a264da87f88e49f1f08676
Markus1k <you@example.com>
2021-12-19 19:54:22 +0200

    created index page markup, add first styles.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,27 @@
 <!--Landing login/register page-->
+
 <script>
+
 
 </script>
 <svelte:head>
-    <title>Ihtiphal</title>
+    <title>Ихтифаль</title>
 </svelte:head>
+<style>
+:global(body) {
+   background-image: url("/src/bg.png");
+   background-repeat: no-repeat;
+   background-size: cover;
+}
+</style>
 
-<h1>Welcome to Ihtiphal, Adventurer!</h1>
-<button>Login</button>
+<div class="font-body bg-white flex justify-center md:h-[25rem] md:w-[25rem] h-[5rem] w-[5rem] mx-auto mt-[15rem] drop-shadow-xl text-center rounded-md">
+    <div>  
+        <h1 class="pt-[3rem] inline-block text-center">Добро пожаловать в Ихтифаль,<br> Приключенец!</h1> 
+        <button class="bg-gradient-to-r from-purple-500 to-pink-500 rounded-md w-[100px] h-[2rem] text-white md:w-[300px] md:h-[3rem] mt-[9rem] hover:shadow-lg transition ease-in-out duration-300 hover:shadow-pink-500/50">Войти</button>
+    
+    </div>
+  
+
+</div>
+
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,12 @@
 module.exports = {
     mode: 'jit',
+    theme: {
+
+        extend: {
+          fontFamily: {
+            'body': ['Press\\ Start\\ 2P', 'sans-serif'],
+          },
+          },
+        },
     purge: ['./src/**/*.svelte'],
   }
