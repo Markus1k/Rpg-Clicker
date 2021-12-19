ac71782f3f1054899f3421937f82dd53ced4a49c
Markus1k <you@example.com>
2021-12-19 20:05:56 +0200

    adapted for mobile

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,13 +12,15 @@
    background-image: url("/src/bg.png");
    background-repeat: no-repeat;
    background-size: cover;
+   min-height: 100%;
+   height: 100vh;
 }
 </style>
 
-<div class="font-body bg-white flex justify-center md:h-[25rem] md:w-[25rem] h-[5rem] w-[5rem] mx-auto mt-[15rem] drop-shadow-xl text-center rounded-md">
+<div class="font-body bg-white flex justify-center md:h-[25rem] md:w-[25rem] h-[25rem] w-[25rem] mx-auto mt-[15rem] drop-shadow-xl text-center rounded-md">
     <div>  
-        <h1 class="pt-[3rem] inline-block text-center">Добро пожаловать в Ихтифаль,<br> Приключенец!</h1> 
-        <button class="bg-gradient-to-r from-purple-500 to-pink-500 rounded-md w-[100px] h-[2rem] text-white md:w-[300px] md:h-[3rem] mt-[9rem] hover:shadow-lg transition ease-in-out duration-300 hover:shadow-pink-500/50">Войти</button>
+        <h1 class="pt-[3rem] inline-block text-center select-none">Добро пожаловать в Ихтифаль,<br> Приключенец!</h1> 
+        <button class="bg-gradient-to-r from-purple-500 to-pink-500 rounded-md w-[300px] h-[3rem] text-white md:w-[300px] md:h-[3rem] mt-[9rem] hover:shadow-lg transition ease-in-out duration-300 hover:shadow-pink-500/50">Войти</button>
     
     </div>
   
