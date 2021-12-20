723a320150576a50a8a6bd278462ce3f18541565
Markus1k <you@example.com>
2021-12-20 15:46:12 +0200

    added login form markup

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -23,20 +23,22 @@
 <div class="font-body  flex justify-center md:h-[25rem] md:w-[25rem] h-[20rem] w-[20rem] mx-auto mt-[15rem] ">
     <div class="drop-shadow-xl text-center rounded-md bg-white">  
         <h1 class="pt-[3rem] inline-block text-center md:text-[16px] text-[14px] select-none">Добро пожаловать в Ихтифаль,<br> Приключенец!</h1> 
-        <button class="bg-gradient-to-r from-purple-500 to-pink-500 rounded-md w-[250px] h-[2.5rem] text-white md:w-[300px] md:h-[3rem] md:mt-[9rem] mt-[5rem] hover:shadow-lg transition ease-in-out duration-300 hover:shadow-pink-500/50">Войти</button>
+        <form class="mt-[2rem]">
+            <div class="field rounded-md">
+                <label for="email">Почта</label>
+                <input type="email" class="w-[300px] md:w-[350px]" name="email"  placeholder="email@mail.com"/>
+                
+            </div>
+            <div class="field rounded-md md:mt-[2rem]">
+                <label for="password" >Пароль</label>
+                <input type="password" class="w-[300px] md:w-[350px]" name="password"  placeholder="password"/>
+            </div>
+        </form>
+        <button class="bg-gradient-to-r from-purple-500 to-pink-500 rounded-md w-[250px] h-[2.5rem] text-white md:w-[300px] md:h-[3rem] md:mt-[3rem] mt-[1rem] hover:shadow-lg transition ease-in-out duration-300 hover:shadow-pink-500/50">Войти</button>
     </div>
-    <!--
-    <form class="invisible">
-        <div class="field rounded-md">
-            <input type="email" name="email"  placeholder="email@mail.com"/>
-            <label for="email">Email</label>
-        </div>
-        <div class="field rounded-md">
-            <input type="password" name="password"  placeholder="password"/>
-            <label for="password">Password</label>
-        </div>
-    </form>
--->
+ 
+    
+
 </div>
 
 <footer class="text-white font-body md:text-[.8rem] text-[.5rem] mt-[5rem] text-center shadow-md md:w-[30rem]  w-[20rem] mx-auto rounded-md select-none">
